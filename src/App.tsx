bbed0cee93ff9f3489feaad9407e6049c82551f3
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2025-02-26 12:32:39 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import ElectricityUsageChart from './components/ElectricityUsage/ElectricityUsageChart';
+import './App.css'
+
+
+
+function App() {
+    return (
+        <div className="app">
+            <ElectricityUsageChart/>
+        </div>
+    )
+}
+
+export default App
