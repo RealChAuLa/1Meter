7c551e2e4e018101b361fc1081f592e7c1cc3e6e
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2025-03-06 02:16:30 +0530

    Implemented Login & Sign Up

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// @ts-ignore
+import { MatDialog } from '@angular/material/dialog';
+import {LoginDialogComponent} from '../login-overlay/login-overlay.component';
 
 @Component({
   selector: 'app-navbar',
@@ -7,12 +10,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  // You can add login/signup methods here
+  currentUser: string | null = null;
+
+  constructor(private dialog: MatDialog) {
+    // You might get this from an auth service in a real app
+    this.currentUser = null;
+  }
+
   login() {
-    console.log('Login clicked');
+    const dialogRef = this.dialog.open(LoginDialogComponent, {
+      width: '450px',
+      panelClass: 'custom-dialog-container',
+      data: { mode: 'login' }
+    });
   }
 
   signup() {
-    console.log('Signup clicked');
+    const dialogRef = this.dialog.open(LoginDialogComponent, {
+      width: '400px',
+      panelClass: 'custom-dialog-container',
+      data: { mode: 'signup' }
+    });
+  }
+
+  logout() {
+    this.currentUser = null;
+    // You would normally call your auth service logout method
+    console.log('User logged out');
   }
 }
